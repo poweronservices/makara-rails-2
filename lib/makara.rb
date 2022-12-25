d55df4e25b38db3896c778e96bda74fbd3eee1bc
Joel Barker <joelbarker@poweron.com>
2022-12-24 16:31:13 -0800

    Remove incompatible code

--- lib/makara.rb
+++ lib/makara.rb
@@ -1,6 +1,6 @@
 require 'active_support'
 require 'makara/version'
-require 'makara/railtie' if defined?(Rails)
+
 module Makara
   autoload :Cache,              'makara/cache'
   autoload :ConfigParser,       'makara/config_parser'
@@ -23,7 +23,6 @@ module Makara
 
   module Logging
     autoload :Logger,     'makara/logging/logger'
-    autoload :Subscriber, 'makara/logging/subscriber'
   end
 
   module Strategies
@@ -31,11 +30,5 @@ module Makara
     autoload :RoundRobin,       'makara/strategies/round_robin'
     autoload :PriorityFailover, 'makara/strategies/priority_failover'
     autoload :ShardAware,       'makara/strategies/shard_aware'
-  end
-end
-
-ActiveSupport.on_load(:active_record) do
-  ActiveRecord::LogSubscriber.log_subscribers.each do |subscriber|
-    subscriber.extend ::Makara::Logging::Subscriber
   end
 end
