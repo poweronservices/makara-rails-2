18d89611e5106f7d365acd104f2dc19be333524d
Joel Barker <joelbarker@poweron.com>
2022-12-24 16:27:16 -0800

    Remove unneeded files

--- spec/cache_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-
-describe Makara::Cache do
-  it 'shows a warning' do
-    expect(Makara::Logging::Logger).to receive(:log).with(/Setting the Makara::Cache\.store won't have any effects/, :warn)
-    described_class.store = :noop
-  end
-end
--- spec/support/schema.rb
+++
@@ -1,9 +0,0 @@
-conn = ActiveRecord::Base.connection
-
-if conn.table_exists? "users"
-  conn.execute("TRUNCATE TABLE users")
-else
-  conn.create_table "users" do |t|
-    t.string "name"
-  end
-end
