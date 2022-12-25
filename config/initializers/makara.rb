0c8179e4c9edb10ed1ec6240d356f14fdf437376
Joel Barker <joelbarker@poweron.com>
2022-12-24 17:03:42 -0800

    Add initializer defining required objects

---
+++ config/initializers/makara.rb
@@ -0,0 +1,21 @@
+require 'makara/logging/logger'
+
+Makara::Logging::Logger.logger = Logger.new(STDERR)
+
+# the following objects are required by Makara
+
+unless defined?(Concurrent)
+  module Concurrent
+    def self.monotonic_time
+      Process.clock_gettime(Process::CLOCK_MONOTONIC, :float_second)
+    end
+  end
+end
+
+unless defined?(Time::RFC2822_DAY_NAME)
+  Time::RFC2822_DAY_NAME = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'].freeze
+end
+
+unless defined?(Time::RFC2822_MONTH_NAME)
+  Time::RFC2822_MONTH_NAME = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'].freeze
+end
