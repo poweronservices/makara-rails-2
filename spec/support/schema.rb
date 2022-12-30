6a6d7cf434f5818e7928006a176748374620f59b
Joel Barker <joelbarker@poweron.com>
2022-12-30 08:30:33 -0800

    Always run SQL create (for test coverage)

--- spec/support/schema.rb
+++ spec/support/schema.rb
@@ -1,14 +1,11 @@
 conn = ActiveRecord::Base.connection
 
-if conn.table_exists? "users"
-  conn.execute("TRUNCATE TABLE users")
-else
-  conn.execute(<<-SQL
-    CREATE TABLE users (
-      id INT NOT NULL AUTO_INCREMENT,
-      name VARCHAR(255),
-      PRIMARY KEY(id)
-    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4
-  SQL
-  )
-end
+conn.execute(<<-SQL
+  CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255),
+    PRIMARY KEY(id)
+  ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4
+SQL
+)
+conn.execute("TRUNCATE TABLE users")
